a51e48af81c1b4bcea7339b7bbf27352357a14cf
Alan <dasilva.alan@outlook.com>
2021-04-10 04:46:54 +0300

    added dependencies

--- app/src/main/java/fi/alandasilva/chat/fragments/group/GroupFragment.kt
+++ app/src/main/java/fi/alandasilva/chat/fragments/group/GroupFragment.kt
@@ -1,4 +1,4 @@
-package fi.alandasilva.chat
+package fi.alandasilva.chat.fragments.group
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -7,25 +7,26 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import androidx.navigation.fragment.findNavController
+import fi.alandasilva.chat.R
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
  */
-class FirstFragment : Fragment() {
+class GroupFragment : Fragment() {
 
     override fun onCreateView(
             inflater: LayoutInflater, container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_first, container, false)
+        return inflater.inflate(R.layout.fragment_group, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         view.findViewById<Button>(R.id.button_first).setOnClickListener {
-            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
+            findNavController().navigate(R.id.action_GroupFragment_to_ChatFragment)
         }
     }
 }
